13e81cf807c7ed1cac1566986de51de90eb6e5ba
Lázaro Ferreira <155861352+Lazaro-fe@users.noreply.github.com>
2026-07-06 19:08:06 +0000

    Salvando

--- Repository/Categoria_repository.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.Avaliacao_Final.Data_base.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projeto.Avaliacao_Final.Data_base.Model.Categoria;
-
-public interface Categoria_repository extends JpaRepository<Categoria, Long>{
-    
-}
--- Repository/Movimentacao_repository.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.Avaliacao_Final.Data_base.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projeto.Avaliacao_Final.Data_base.Model.Movimentacao;
-
-public interface Movimentacao_repository  extends JpaRepository<Movimentacao, Long>{
-    
-}
